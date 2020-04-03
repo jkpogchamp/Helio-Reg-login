a3422e38710d630e91756a138dcc3fb630d4fef2
jkpogchamp <jkpogchamper@gmail.com>
2020-04-03 11:06:39 +0200

    registration controller: username changed to email

--- src/controllers/registrationController.js
+++ src/controllers/registrationController.js
@@ -2,12 +2,12 @@ const inputValidator = require('../utils/inputValidationRegister');
 const userService = require('../services/userService');
 
 const register = (request, response) => {
-  const { username, password } = request.body;
-  if (inputValidator(username, password)) {
+  const { email, password } = request.body;
+  if (inputValidator(email, password)) {
     response.status(401).send({ message: inputValidator.message });
   }
   userService
-    .register(username, password)
+    .register(email, password)
     .then(data => response.status(200).json(data))
     .catch(error => response.status(409).json({ message: error.message }));
 };
